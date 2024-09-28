28eb7299941ccec322413260372007b3ee7c3796
Adrián Vico Hernández <1hiaw.vico.adrian@gmail.com>
2024-09-28 10:00:15 +0200

    Creado handler y reordenodao con carpetas en Ejercicios C2.1

---
+++ EjerciciosC21/Handlers/LoaderHandler.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjerciciosC21
+{
+    public class LoaderHandler
+    {
+        // Método void para inciar método de potencia
+        public static void PowLoader()
+        {
+            int numBase, numPow;
+            bool validInt = false;
+            String inputString = "";
+            while (inputString != "exit")
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Introduce el primer número int de base o escribe 'exit' para cancelar");
+                Console.WriteLine("");
+                Console.Write("EJERCICIOS C2.1 > POTENCIA > ");
+                inputString = Console.ReadLine();
+                validInt = int.TryParse(inputString, out numBase);
+                if (validInt)
+                {
+                    while (inputString != "exit")
+                    {
+                        Console.WriteLine("");
+                        Console.WriteLine("Introduce el segundo número int de potencia o escribe 'exit' para cancelar");
+                        Console.WriteLine("");
+                        Console.Write("EJERCICIOS C2.1 > POTENCIA > BASE " + numBase + " > ");
+                        inputString = Console.ReadLine();
+                        validInt = int.TryParse(inputString, out numPow);
+                        if (validInt)
+                        {
+                            Console.WriteLine("");
+                            Console.WriteLine("----------------------------------------------------");
+                            Console.WriteLine("///// " + numBase + " elevado a " + numPow + " es igual a " + NumberController.PowerOperation(numBase, numPow));
+                            Console.WriteLine("----------------------------------------------------");
+                            Console.WriteLine("");
+                            inputString = "exit";
+                        }
+                        else if (inputString != "exit")
+                        {
+                            Console.WriteLine("Input o número no válido");
+                            Console.WriteLine("");
+                        }
+                    }
+                    inputString = "";
+                }
+                else if (inputString != "exit")
+                {
+                    Console.WriteLine("Input o número no válido");
+                    Console.WriteLine("");
+                }
+            }
+        }
+    }
+}
--- EjerciciosC21/Program.cs
+++ EjerciciosC21/Program.cs
@@ -8,54 +8,6 @@ namespace EjerciciosC21
 {
     internal class Program
     {
-        // Método void para inciar método de potencia
-        static void PowLoader()
-        {
-            int numBase, numPow;
-            bool validInt = false;
-            String inputString = "";
-            while (inputString != "exit")
-            {
-                Console.WriteLine("");
-                Console.WriteLine("Introduce el primer número int de base o escribe 'exit' para cancelar");
-                Console.WriteLine("");
-                Console.Write("EJERCICIOS C2.1 > POTENCIA > ");
-                inputString = Console.ReadLine();
-                validInt = int.TryParse(inputString, out numBase);
-                if (validInt)
-                {
-                    while (inputString != "exit")
-                    {
-                        Console.WriteLine("");
-                        Console.WriteLine("Introduce el segundo número int de potencia o escribe 'exit' para cancelar");
-                        Console.WriteLine("");
-                        Console.Write("EJERCICIOS C2.1 > POTENCIA > BASE " + numBase + " > ");
-                        inputString = Console.ReadLine();
-                        validInt = int.TryParse(inputString, out numPow);
-                        if (validInt)
-                        {
-                            Console.WriteLine("");
-                            Console.WriteLine("----------------------------------------------------");
-                            Console.WriteLine("///// " + numBase + " elevado a " + numPow + " es igual a " + NumberController.PowerOperation(numBase, numPow));
-                            Console.WriteLine("----------------------------------------------------");
-                            Console.WriteLine("");
-                            inputString = "exit";
-                        }
-                        else if (inputString != "exit")
-                        {
-                            Console.WriteLine("Input o número no válido");
-                            Console.WriteLine("");
-                        }
-                    }
-                    inputString = "";
-                }
-                else if (inputString != "exit")
-                {
-                    Console.WriteLine("Input o número no válido");
-                    Console.WriteLine("");
-                }
-            }
-        }
         static void Main(string[] args)
         {
             string option = "";
@@ -68,7 +20,7 @@ namespace EjerciciosC21
                 switch (option)
                 {
                     case "pow":
-                        PowLoader();
+                        LoaderHandler.PowLoader();
                         option = "";
                         break;
                     default:
